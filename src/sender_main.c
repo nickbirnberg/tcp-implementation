2db2eda1d81912055e945cd938dea682bb3bef65
Nick Birnberg <nbirnberg@gmail.com>
2015-10-07 23:31:28 -0500

    Just saw beejs guide

--- src/sender_main.c
+++ src/sender_main.c
@@ -7,12 +7,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
-void reliablyTransfer(char* hostname, unsigned short int hostUDPport, char* filename, unsigned long long int bytesToTransfer);
+#define MAXPAYLOAD 1472 // max number of bytes we can get at once
+
+void reliablyTransfer(char* hostname, char* hostUDPport, char* filename, unsigned long long int bytesToTransfer);
 
 int main(int argc, char** argv)
 {
-	unsigned short int udpPort;
+	char* udpPort;
 	unsigned long long int numBytes;
 	
 	if(argc != 5)
@@ -20,8 +30,57 @@ int main(int argc, char** argv)
 		fprintf(stderr, "usage: %s receiver_hostname receiver_port filename_to_xfer bytes_to_xfer\n\n", argv[0]);
 		exit(1);
 	}
-	udpPort = (unsigned short int)atoi(argv[2]);
+	udpPort = argv[2];
 	numBytes = atoll(argv[4]);
 	
 	reliablyTransfer(argv[1], udpPort, argv[3], numBytes);
+	return 0;
 } 
+
+void reliablyTransfer(char* hostname, char* hostUDPport, char* filename, unsigned long long int bytesToTransfer)
+{
+	int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    if ((rv = getaddrinfo(hostname, hostUDPport, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return;
+    }
+
+    // loop through all the results and make a socket
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("talker: socket");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "talker: failed to create socket\n");
+        return;
+    }
+
+    char message[] = "Hello!";
+
+    if ((numbytes = sendto(sockfd, message, strlen(message), 0,
+             p->ai_addr, p->ai_addrlen)) == -1) {
+        perror("talker: sendto");
+        exit(1);
+    }
+
+    freeaddrinfo(servinfo);
+
+    printf("talker: sent %d bytes to %s\n", numbytes, hostname);
+    close(sockfd);
+
+    return;
+}
